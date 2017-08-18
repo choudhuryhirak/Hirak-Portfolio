158e7f87e815219fe10d82c3ea86d9790495511d
Hirak Choudhury <choudhuryhirakkumar@gmail.com>
2017-08-18 11:35:11 +0530

    beforelive

--- postcontact.php
+++ postcontact.php
@@ -2,22 +2,22 @@
 include("db.php");
 $pagename="Contact Confirmed";
 
-echo "<title>".$pagename."</title>";
+
 
 $fname=$_POST['name'];
-echo 'first name is' .$fname.'<br>';
+
 
 $lname=$_POST['lname'];
-echo 'last name is' .$lname.'<br>';
+
 
 $email=$_POST['email'];
-echo 'email name is' .$email.'<br>';
+
 
 $message=$_POST['message'];
 $inmessage=mysqli_real_escape_string($conn, $message);
-echo "brief is" .$inmessage. "<br>";
 
-echo "<br><h4>Your message has been submitted</h4>";
+
+
 
 $SQLB="insert into messa
         (FirstName, LastName, Email, Message)
@@ -25,5 +25,8 @@ $SQLB="insert into messa
             ('".$fname."', '".$lname."', '".$email."', '".$inmessage."')";
         
 $exeSQLB=mysqli_query($conn, $SQLB) or die(mysqli_error($conn));
+include 'thankscontact.html';
+
+
 
 ?>
--- quote.php
+++ quote.php
@@ -2,32 +2,32 @@
 include("db.php");
 $pagename="Contact Site";
 
-echo "<title>".$pagename."</title>";
+
 
 $fname=$_POST['firstname'];
-echo 'first name is' .$fname.'<br>';
+
 
 $lname=$_POST['lastname'];
-echo 'last name is' .$lname.'<br>';
+
 
 $email=$_POST['email'];
-echo 'email name is' .$email.'<br>';
+
 
 $phone=$_POST['phonenumber'];
-echo 'number is' .$phone.'<br>';
+
 
 
 $services=implode(',', $_POST['se']);
-echo "services are" .$services."<br>";
+
 
 $probrief=$_POST['message'];
 $inprobrief=mysqli_real_escape_string($conn, $probrief);
-echo "brief is" .$inprobrief. "<br>";
+
 
 $estimate=$_POST['estimate'];
-echo 'estimate is' .$estimate.'<br>';
 
-echo "<br><h4>Your message has been submitted</h4>";
+
+
 
 $SQLA="insert 
 into users
@@ -37,7 +37,6 @@ into users
         
 $exeSQLA=mysqli_query($conn, $SQLA) or die(mysqli_error($conn));
 
-echo "<br><h4>Your message has been submitted</h4>";
-
+include 'thanksquote.html';
 
 ?>
